9d8222c859a0d5ad9c6c1bd526eac283573a3ad3
CamilaDLRS <mila.delarosa@hotmail.com>
2024-02-05 14:11:36 -0300

    feat: createEventSchema

--- src/middlewares/schemas/event.schema.ts
+++ src/middlewares/schemas/event.schema.ts
@@ -1,19 +1,26 @@
 import * as yup from 'yup';
-
-const validOrderKeys = ['deposit', 'withdraw'];
+import { EventTypes } from '../../entities/accountEvent';
 
 export const createEventSchema = yup.object({
   body: yup.object({
     type: yup
     .string()
-    .oneOf(validOrderKeys)
+    .oneOf<EventTypes>(['deposit', 'withdraw', 'transfer'])
     .required("Tipo de evento é obrigatório."),
     destination: yup
     .string()
-    .required("Identificação da conta de destino é obrigatória."),
+   ,// .required("Identificação da conta de destino é obrigatória."),
+   origin: yup
+   .string()
+  ,// .required("Identificação da conta de origem é obrigatória."),
     amount: yup
     .number()
     .positive()
     .required("Valor do evento é obrigatório.")
   })
 });
+/* 
+se deposito, destino obrigatirio
+se withdraw origem obrigatriro
+se transferencaia origem obrigatriro
+ */
